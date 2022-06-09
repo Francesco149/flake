39353d63da1e22baac7547f971d5cd076883ec6c
Franc[e]sco <lolisamurai@tfwno.gf>
2022-06-09 14:20:45 +0200

    emacs: formatting

--- emacs/init.el
+++ emacs/init.el
@@ -287,8 +287,8 @@
            (aw-switch-to-window (aw-select nil))
            (call-interactively (symbol-function ',fn)))))))
 
-(define-key embark-file-map     (kbd "o") (loli/embark-ace-action find-file))
-(define-key embark-buffer-map   (kbd "o") (loli/embark-ace-action switch-to-buffer))
+(define-key embark-file-map (kbd "o") (loli/embark-ace-action find-file))
+(define-key embark-buffer-map (kbd "o") (loli/embark-ace-action switch-to-buffer))
 (define-key embark-bookmark-map (kbd "o") (loli/embark-ace-action bookmark-jump))
 
 ;; avy: qutebrowser-like movement hints
