70a81400d4359d1c02fce0566982eab2c7a0259f
Franc[e]sco <lolisamurai@tfwno.gf>
2022-06-12 15:29:43 +0200

    remove NUR, I dont need it anymore

--- flake.nix
+++ flake.nix
@@ -7,38 +7,27 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs"; # updated more frequently than home-manager's fork
     };
-    nurpkgs = {
-      url = github:nix-community/NUR;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    loli.url = "git+ssh://git@github.com/Francesco149/nur-packages.git";
   };
 
-  outputs = { self, nixpkgs, home-manager, nurpkgs, loli }:
+  outputs = { self, nixpkgs, home-manager }:
   let
     user = "loli";
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
       config.allowUnfree = true;
-      overlays = [ nurpkgs.overlay ];
-    };
-    nur = import nurpkgs {
-      inherit pkgs;
-      nurpkgs = pkgs;
-      repoOverrides = { loli = import loli { inherit pkgs; }; };
     };
   in {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         inherit system;
-        specialArgs = { inherit user; inherit nur; };
+        specialArgs = { inherit user; };
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit user; inherit nur; };
+            home-manager.extraSpecialArgs = { inherit user; };
             home-manager.users.${user} = {
               imports = [ ./home.nix ];
             };
