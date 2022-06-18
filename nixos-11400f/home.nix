ae1394691b8f2502e573f68502fbc246d29b7e77
Franc[e]sco <lolisamurai@tfwno.gf>
2022-06-18 13:56:10 +0200

    fix secrets path

--- nixos-11400f/home.nix
+++ nixos-11400f/home.nix
@@ -321,7 +321,7 @@ in with config; {
   home.sessionVariables.DEFAULT_BROWSER = "${firefox-custom}/bin/firefox";
 
   # NOTE: private config files. comment out or provide your own
-  xdg.configFile."gh2md/token".source = ./secrets/gh2md/token;
-  home.file.".gist".source = ./secrets/gist/token;
+  xdg.configFile."gh2md/token".source = ../secrets/gh2md/token;
+  home.file.".gist".source = ../secrets/gist/token;
 
 }
