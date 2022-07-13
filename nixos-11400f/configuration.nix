d870b69493c42ec5c0ca1a7cda5d8577efec52c5
Franc[e]sco <lolisamurai@tfwno.gf>
2022-07-13 11:38:04 +0200

    11400f: add samba

--- nixos-11400f/configuration.nix
+++ nixos-11400f/configuration.nix
@@ -411,6 +411,10 @@ in
       443 # https
       synapsePort
       dendritePort
+      5357 # wsdd, for samba win10 discovery
+    ];
+    allowedUDPPorts = [
+      3702 # wsdd, for samba win10 discovery
     ];
   };
 
@@ -895,6 +899,48 @@ in
       dendriteDomain
       synapseDomain
     ];
+  };
+
+  services.samba-wsdd.enable = true; # make shares visible for windows 10 clients
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    securityType = "user";
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = smbnix
+      netbios name = smbnix
+      security = user
+      #use sendfile = yes
+      #max protocol = smb2
+      # note: localhost is the ipv6 localhost ::1
+      hosts allow = 192.168.1. 127.0.0.1 localhost
+      hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+    '';
+    shares = {
+      public = {
+        path = "/mnt/Shares/Public";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "username";
+        "force group" = "groupname";
+      };
+      private = {
+        path = "/mnt/Shares/Private";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "username";
+        "force group" = "groupname";
+      };
+    };
   };
 
   # NOTE: private config files. comment out or provide your own
