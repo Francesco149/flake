2d24788ea01d07e49e493b5624016c3ef6a6b232
Franc[e]sco <lolisamurai@tfwno.gf>
2022-07-13 11:43:17 +0200

    i1400f: samba: WHY DO I REPEAT MYSELF

--- nixos-11400f/configuration.nix
+++ nixos-11400f/configuration.nix
@@ -927,7 +927,7 @@ in
         "guest ok" = "yes";
         "create mask" = "0644";
         "directory mask" = "0755";
-        "force user" = "loli";
+        "force user" = "${user}";
         "force group" = "users";
       };
       private = {
@@ -937,7 +937,7 @@ in
         "guest ok" = "no";
         "create mask" = "0644";
         "directory mask" = "0755";
-        "force user" = "loli";
+        "force user" = "${user}";
         "force group" = "users";
       };
     };
