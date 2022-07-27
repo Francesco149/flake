38ef3097f99c1fd5974d561f53eb366829f2ec0c
Franc[e]sco <lolisamurai@tfwno.gf>
2022-07-27 15:11:57 +0200

    emacs: add function to clear undo-tree

--- emacs/init.el
+++ emacs/init.el
@@ -162,6 +162,24 @@
 (setq undo-tree-auto-save-history t)
 (setq undo-tree-enable-undo-in-region nil)
 
+(defun clean-undo-tree ()
+"Clear current buffer's undo-tree.
+Undo-tree can cause problems with file encoding when characters
+are inserted that cannot be represented using the files current
+encoding. This is even the case when characters are only
+temporarily inserted, e.g. pasted from another source and then
+instantly deleted. In these situations it can be necessary to
+clear the buffers undo-tree before saving the file."
+  (interactive)
+  (let ((buff (current-buffer)))
+    (if (local-variable-p 'buffer-undo-tree)
+        (if (y-or-n-p "Clear buffer-undo-tree? ")
+            (progn
+              (setq buffer-undo-tree nil)
+              (message "Cleared undo-tree of buffer: %s" (buffer-name buff)))
+          (message "Cancelled clearing undo-tree of buffer: %s" (buffer-name buff)))
+      (error "Buffer %s has no local binding of `buffer-undo-tree'" (buffer-name buff)))))
+
 ;; dired settings (file browser)
 (setq dired-listing-switches "-agho --group-directories-first")
 (require 'dired-single) ; single buffer for dired
