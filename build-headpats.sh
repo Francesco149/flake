b3e332e3b7184ca1b1e3c80df0a1479f8d58d2da
loli <loli@headpats.uk>
2022-10-31 01:34:29 +0100

    headpats: build remotely

--- build-headpats.sh
+++ build-headpats.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nixos-rebuild --flake .#headpats --target-host root@headpats.uk --build-host localhost switch
+nixos-rebuild --flake .#headpats --target-host root@headpats.uk --build-host root@headpats.uk switch
