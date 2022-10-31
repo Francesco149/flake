3a9dc0a0e0b6b02353e61a6632029bf94aa4a820
loli <loli@headpats.uk>
2022-10-31 01:28:02 +0100

    headpats: auto gc

--- headpats/configuration.nix
+++ headpats/configuration.nix
@@ -20,6 +20,10 @@ in {
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpNgs8JFiW2okM8bWoQXkXD6y3x1LONA3hNQbUmvJhMK8BP7Ajkd5avC0dhyOnHee1WCoiQfCfqN/2SVgHMDmRv2QNluciZ4scFr1IwXRxrUqRPpDid6bBIc/e7PYcFBfA2r1nfOdZTePiQcQAcb0yhblqtsg9aOgl+JwqK4GvoQgwriB3Hp6PrezRYBcQjjLbcrU8U1vqKCljhL/cYy5qj5ybJ4hRYcsuZoiQxjtomlrsmibVcTJZVnwPL3DVhCcNrPYABstVgLZfLSttCQCdB2VvGJOx5r6gaB8bkgHsqgERyZza4hBYsMPLSuzxrxgEH+AZzTBGIZiWD0WgY+81 loli@void" 
   ];
 
+  # automatically garbage collect nix store to save disk space
+  nix.gc.automatic = true;
+  nix.gc.dates = "03:15";
+
   security.acme.acceptTerms = true;
   security.acme.defaults.email = "francesco149@gmail.com";
 
