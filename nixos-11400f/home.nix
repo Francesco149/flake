489a5276b9dc40ffe9706679dedcd58d0fa2ddcd
loli <loli@headpats.uk>
2022-11-27 14:05:21 +0100

    kdenlive -> olive-editor

--- nixos-11400f/home.nix
+++ nixos-11400f/home.nix
@@ -92,6 +92,7 @@ in with config; {
     krita
     tor-browser-bundle-bin
     quickemu
+    olive-editor
 
     (pkgs.writeShellScriptBin "cam" ''
       mpv --profile=low-latency --untimed $(ls /dev/video* | dmenu)
