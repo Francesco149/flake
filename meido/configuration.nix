cd2fd8e688b2c0663353528d19e33bbd06f67e5b
loli <loli@headpats.uk>
2022-12-22 16:56:38 +0100

    meido: add dnscrypt

--- meido/configuration.nix
+++ meido/configuration.nix
@@ -30,10 +30,34 @@
   networking = {
     hostName = "meido";
     usePredictableInterfaceNames = false;
-    nameservers = [ "8.8.8.8" ];
+    nameservers = [ "127.0.0.1" "::1" ];
     defaultGateway = "192.168.1.1";
     resolvconf.enable = false;
     dhcpcd.enable = false;
+  };
+
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      ipv6_servers = true;
+      require_dnssec = true;
+
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+
+      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+      # server_names = [ ... ];
+    };
+  };
+
+  systemd.services.dnscrypt-proxy2.serviceConfig = {
+    StateDirectory = "dnscrypt-proxy";
   };
 
   networking.interfaces.eth0.ipv4.addresses = [{
