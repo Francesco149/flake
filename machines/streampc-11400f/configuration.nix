45f615d869873da3e82e67b5fd20c59b7cd0197d
lolisamurai <loli@headpats.uk>
2024-10-08 15:26:09 +0200

    11400f: fix configuration.nix because i am a copy paste idiot

--- machines/streampc-11400f/configuration.nix
+++ machines/streampc-11400f/configuration.nix
@@ -1,18 +1,10 @@
 { ... }:
 {
   imports = [
-    # nixos-generate-config --show-hardware-config > hardware-configuration.nix
     ./hardware-configuration.nix
-    ../../common/boot/configuration.nix
-    ../../common/users/configuration.nix
-    ../../common/nix/configuration.nix
-    ../../common/mitigations/configuration.nix
-    ../../common/locale/configuration.nix
-    ../../common/ssh/configuration.nix
-    ../../common/hosts/configuration.nix
-    ../../common/autologin/configuration.nix
     ../../common/nvidia/configuration.nix
+    ../../common/streampc/configuration.nix
   ];
 
-  system.stateVersion = "24.05";
+  system.stateVersion = "23.05";
 }
