f6b1ec8ebaeb3110729bdc3372219f62370639dc
lolisamurai <loli@headpats.uk>
2024-11-16 11:49:41 +0100

    desktop: move tanuki secrets out of the common config

--- machines/tanuki/configuration.nix
+++ machines/tanuki/configuration.nix
@@ -16,23 +16,59 @@
   # secrets
 
   # by default, agenix does not look in your home dir for keys
+  # TODO: do not hardcode this home path, get it from home-manager somehow or something
   age.identityPaths = [
-    "/root/.ssh/id_rsa"
+    "/home/${user}/.ssh/id_rsa"
   ];
 
   # agenix secrets are owned by root and symlinked from /run/agenix.d .
-  # to have user-owned secrets I have to disable symlinking and make sure the ownership is correct
-  # TODO: don't hardcode barrier folder
-  age.secrets = {
+  # so to have user-owned secrets I have to disable symlinking and make sure the ownership is correct
+  age.secrets =
+    let
 
-    barrierc-private-key = {
-      file = ../../secrets/barrier/BarrierTanuki.pem.age;
-      path = "/home/${user}/.local/share/barrier/SSL/Barrier.pem";
-      symlink = false;
-      owner = "loli";
+      mkUserSecret = { file, path }: {
+        inherit file path;
+        owner = "${user}";
+        group = "users";
+        symlink = false;
+      };
+
+      secretPath = path: "/var/lib/${user}-secrets/${path}";
+
+    in
+    {
+
+      barrierc-private-key = mkUserSecret {
+        file = ../../secrets/barrier/BarrierTanuki.pem.age;
+        path = "/home/${user}/.local/share/barrier/SSL/Barrier.pem";
+      };
+
+      gh2md-token = mkUserSecret {
+        file = ../../secrets/gh2md/token.age;
+        path = "/home/${user}/.config/gh2md/token";
+      };
+
+      gist-token = mkUserSecret {
+        file = ../../secrets/gist/token.age;
+        path = "/home/${user}/.gist";
+      };
+
+      chatterino-settings = mkUserSecret {
+        file = ../../secrets/chatterino/settings.json.age;
+        path = "/home/${user}/.local/share/chatterino/Settings/settings.json";
+      };
+
+      protonvpn-creds = mkUserSecret {
+        file = ../../secrets/protonvpn/creds.txt.age;
+        path = "/home/${user}/.local/share/protonvpn/creds.txt";
+      };
+
+      protonvpn-conf = mkUserSecret {
+        file = ../../secrets/protonvpn/config.ovpn.age;
+        path = "/home/${user}/.local/share/protonvpn/config.ovpn";
+      };
+
     };
-
-  };
 
   system.stateVersion = "22.11";
 
