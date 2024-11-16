8397d2018e028fd7588ae7656da2bfd52c868e5b
lolisamurai <loli@headpats.uk>
2024-11-16 13:10:50 +0100

    zen: update gnome shortcuts

--- machines/zen/home.nix
+++ machines/zen/home.nix
@@ -4,5 +4,13 @@
     ../../common/desktop/home.nix
   ];
 
+    settings."org/gnome/shell".favorite-apps = [
+      "firefox.desktop"
+      "xterm.desktop"
+      "org.gnome.Nautilus.desktop"
+      "flowblade.desktop"
+      "org.gnome.TextEditor.desktop"
+    ];
+
   home.stateVersion = "24.05";
 }
