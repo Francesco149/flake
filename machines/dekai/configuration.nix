70b93dd3a6e234b7f1a28e7f28981f2e99f5aad8
lolisamurai <loli@headpats.uk>
2024-12-01 14:35:48 +0100

    dekai: enable tuya_local, fix default_config not applying

--- machines/dekai/configuration.nix
+++ machines/dekai/configuration.nix
@@ -302,15 +302,20 @@ in
   services.home-assistant = {
     enable = true;
     extraComponents = [
+      # Includes dependencies for a basic setup
+      # https://www.home-assistant.io/integrations/default_config/
+      "default_config"
+
       # Components required to complete the onboarding
       "esphome"
       "met"
       "radio_browser"
     ];
+    customComponents = with pkgs.home-assistant-custom-components; [
+      tuya_local
+    ];
     config = {
-      # Includes dependencies for a basic setup
-      # https://www.home-assistant.io/integrations/default_config/
-      default_config = {};
+
     };
   };
 
