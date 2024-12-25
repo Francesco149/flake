c388142d0bd83cbb857d33ad2e1582c6f4341f9c
lolisamurai <loli@headpats.uk>
2024-12-25 20:12:01 +0100

    desktop: enable ntfs

--- common/boot/configuration.nix
+++ common/boot/configuration.nix
@@ -3,4 +3,5 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.consoleMode = "max"; # hdmi switch is happier if we dont switch res during boot
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "ntfs" ];
 }
