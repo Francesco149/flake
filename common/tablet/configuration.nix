cf412871e1b9ad9e39c8c7df0104719ccd5b2d43
lolisamurai <loli@headpats.uk>
2025-02-01 16:03:30 +0100

    update flake

--- common/tablet/configuration.nix
+++ common/tablet/configuration.nix
@@ -1,9 +1,5 @@
 { config, pkgs, ... }:
 {
-  environment.systemPackages = [
-    config.boot.kernelPackages.digimend
-  ];
-
   services.xserver = {
     modules = [ pkgs.xf86_input_wacom ];
     wacom.enable = true;
