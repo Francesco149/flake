f30facf94965268ec8e373d78eae1a8d0059bdbc
lolisamurai <loli@headpats.uk>
2025-12-12 01:09:39 +0100

    dekai: update hw config

--- machines/dekai/hardware-configuration.nix
+++ machines/dekai/hardware-configuration.nix
@@ -5,39 +5,30 @@
 
 {
   imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "mpt3sas" "xhci_pci" "ahci" "nvme" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "mpt3sas" "xhci_pci" "ahci" "nvme" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/7edabab8-f32f-4732-8e4c-e1fd340c423f";
+    { device = "/dev/mapper/luks-c7f9b615-5e1c-4015-8824-b17fbce9b7cc";
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."luks-f93f646c-38b5-455c-973b-d8e548b32e96".device = "/dev/disk/by-uuid/f93f646c-38b5-455c-973b-d8e548b32e96";
+  boot.initrd.luks.devices."luks-c7f9b615-5e1c-4015-8824-b17fbce9b7cc".device = "/dev/disk/by-uuid/c7f9b615-5e1c-4015-8824-b17fbce9b7cc";
 
   fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/ECB0-6790";
+    { device = "/dev/disk/by-uuid/F08D-5007";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/4a23e9c0-f796-46f6-acc5-886620bc20b0"; }];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
+    [ { device = "/dev/mapper/luks-18ebd863-bc36-473e-a37b-1574c9357791"; }
+    ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
